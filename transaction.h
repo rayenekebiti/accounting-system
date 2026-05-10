7040418b848f9cce500f85d1bf64fa3f78693c38
rayenekebiti <kebitirayene@gmail.com>
2026-05-10 08:33:26 +0800

    finished declaring all the methodes needed for base class, used a struct to make bulk getter and setter method

--- transaction.h
+++ transaction.h
@@ -4,6 +4,7 @@
 class Transaction
 {
     protected:
+    struct Transaction_info{
     unsigned short int id;
     char description[MAX_DESCRIPTION_LENGTH];
     double amount;
@@ -11,14 +12,19 @@ class Transaction
     unsigned short int category_id;
     TransactionType type;
     bool isDeleted;
+    };
+    Transaction_info default_Transaction; 
     public:
-    virtual void getEffectiveAmount()=0;
-    virtual void getType()=0;
+    virtual ~Transaction()=default;
+    virtual double getEffectiveAmount()=0;
+    virtual TransactionType getType()=0;
     virtual void display()=0;
     virtual void serialize()=0;
     virtual void deserialize()=0;
-    unsigned short int getter(unsigned short int get_Transaction_id) const;
-    void setter(const unsigned short int set_Transaction_id);
+     Transaction_info Transaction_info_getter() const;
+     //{return default_Transaction}
+    void Transaction_info_setter(const Transaction_info& set_Transaction_info);
+    //{deault_Transaction=set_Transaction}
 };
 
 #endif
