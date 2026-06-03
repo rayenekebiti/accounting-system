28533d94428e742ab675110a09f07213af22c79f
rayenekebiti <kebitirayene@gmail.com>
2026-06-03 18:46:34 +0800

    organized the transaction files

---
+++ Videos/Captures/PROJECTS/accounting-system/transaction.h
@@ -0,0 +1,47 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include "constants.h"
+
+class Transaction
+{
+protected:   
+    unsigned short int id;
+    char description[MAX_DESCRIPTION_LENGTH];
+    double amount;
+    char date[MAX_DATE_LENGTH];
+    unsigned short int categoryId;
+    TransactionType type;
+    bool isDeleted;
+
+
+public:
+    Transaction(){};
+    Transaction(const Transaction& copy_Transaction){};
+    Transaction(Transaction&& move_Transaction){};
+    virtual ~Transaction() = default;
+    virtual double getEffectiveAmount() const = 0;
+    virtual TransactionType getType() const = 0;
+    virtual void display() const = 0;
+    virtual void serialize(char* buffer) const = 0;
+    virtual void deserialize(const char* buffer) = 0;
+
+    unsigned short int getId() const;
+    void setId(const unsigned short int newId);
+
+    const char* getDescription() const;
+    void setDescription(const char* newDescription);
+
+    double getAmount() const;
+    void setAmount(const double newAmount);
+
+    const char* getDate() const;
+    void setDate(const char* newDate);
+
+    unsigned short int getCategoryId() const;
+    void setCategoryId(const unsigned short int newCategoryId);
+
+    bool getIsDeleted() const;
+    void setIsDeleted(const bool newIsDeleted);
+};
+
+#endif
